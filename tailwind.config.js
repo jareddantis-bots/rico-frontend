61104010c39161179a4c17f0339cedc128840726
Jared Dantis <jareddantis@gmail.com>
2022-08-11 17:32:45 +0800

    tailwind.config: Silence warnings about renamed colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
+delete colors['lightBlue'];
+delete colors['warmGray'];
+delete colors['trueGray'];
+delete colors['coolGray'];
+delete colors['blueGray'];
+
 module.exports = {
   theme: {
     extend: {},
